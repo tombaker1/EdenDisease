fddf55fc98cdcafa16bfab545888dc458d1e4f66
Tom Baker <tombaker1@gmail.com>
2014-04-24 07:59:44 -0700

    Adding more files

---
+++ www/js/xformer.js
@@ -0,0 +1,222 @@
+/*!
+ * jQuery lightweight plugin boilerplate
+ * Original author: @ajpiano
+ * Further changes, comments: @addyosmani
+ * Licensed under the MIT license
+ */
+
+
+// the semi-colon before the function invocation is a safety 
+// net against concatenated scripts and/or other plugins 
+// that are not closed properly.
+;(function ( $, window, document, undefined ) {
+    
+    // undefined is used here as the undefined global 
+    // variable in ECMAScript 3 and is mutable (i.e. it can 
+    // be changed by someone else). undefined isn't really 
+    // being passed in so we can ensure that its value is 
+    // truly undefined. In ES5, undefined can no longer be 
+    // modified.
+    
+    // window and document are passed through as local 
+    // variables rather than as globals, because this (slightly) 
+    // quickens the resolution process and can be more 
+    // efficiently minified (especially when both are 
+    // regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = 'xformer',
+        defaults = {
+            propertyName: "value"
+        };
+    
+    // create the form list item  
+    function formItem(name,url) {
+        this.url = url,
+        this.name = name,
+        this.loaded = false;
+        this.form = null;
+        this.model = null;
+    }
+    var formList = [];
+    
+    // create the query state
+    var reqState = null,
+        reqTimer = null,
+        reqIndex = 0,
+        reqCompleteCB = null,
+        REQ_WAIT_TIME = 4000;
+
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+
+        // jQuery has an extend method that merges the 
+        // contents of two or more objects, storing the 
+        // result in the first object. The first object 
+        // is generally empty because we don't want to alter 
+        // the default options for future instances of the plugin
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+       
+        this.init();
+    };
+    
+    Plugin.prototype.init = function () {
+        // Place initialization logic here
+        // You already have access to the DOM element and
+        // the options via the instance, e.g. this.element 
+        // and this.options
+        reqState = new XMLHttpRequest();
+    };
+    
+    Plugin.prototype.getForm = function (i) {
+        return formList[i];
+    }
+    
+    Plugin.prototype.numForms = function (i) {
+        return formList.length;
+    }
+    
+    Plugin.prototype.getDoc = function () {
+        return formList[i];
+    }
+    
+    Plugin.prototype.cbReadFormList = function (reply) {
+        clearTimeout(reqTimer);
+        if (reqState.readyState != 4) {
+            alert("What?");
+            return;
+        }
+        if (reqState.status != 200) {
+            alert("Error loading page");
+            return;
+        }
+        
+        var xmlDoc = $.parseXML(reply.target.responseText);
+        var $xml = $( xmlDoc );
+        forms = $xml.find( "form" );
+        for (var i = 0; i < forms.length; i++) {
+            var $item = $(forms[i]);
+            var name = $item.html();
+            var url = $item.attr("url");
+            formList.push(new formItem(name,url));
+            
+            //console.log(formList[i].name);
+            //$("#formList").append(name + "<br>");
+        }
+        
+        // return and show the form
+        reqCompleteCB();
+        //console.log("cbReadFormList done");
+        
+    };
+    
+    Plugin.prototype.cbReadForm = function (reply) {
+        clearTimeout(reqTimer);
+        //console.log("cbReadFormList done");
+        if (reqState.readyState != 4) {
+            alert("What?");
+            return;
+        }
+        if (reqState.status != 200) {
+            alert("Error loading page");
+            return;
+        }
+        
+        var xmlDoc = $.parseXML(reply.target.responseText);
+        var $xml = $( xmlDoc );
+        
+        // Parse the model
+        var $model = $xml.find("model");
+        var instance = $model.find("instance");
+        var elementName = "";
+        var fields = {};
+        var modelPrototype = {};
+        for (var i = 0; i < instance.length; i++) {
+            var element = $(instance[i]).children()[0];
+            var elementName = element.nodeName;
+            //console.log("element name " + elementName);
+            var fieldItems = $(element).children();
+            
+            // 
+            for (var j = 0; j < fieldItems.length; j++) {
+                // get model element
+                var key = fieldItems[j].nodeName;
+                var value = fieldItems[j].innerHTML;
+                var field = {};
+                var item = {};
+                field["value"] = value;
+                modelPrototype[key] = value;
+                
+                // find binding
+                var nodeset = '/' + elementName + '/' + key;
+                var searchString = "bind[nodeset*='" + nodeset + "']";
+                var bindElement = $model.find(searchString)[0];
+                var attributes = bindElement.attributes;
+                //console.log("attr " + attributes.length);
+               
+                // add attributes of bind
+                var attributeList = {};
+                for (var k = 0; k < attributes.length; k++) {
+                    var name = attributes[k].nodeName;
+                    var value = attributes[k].nodeValue;
+                    field[name] = value;
+                    //console.log('\t' + name + ':' + value);
+                }
+                                
+                // add to array
+                fields[key] = field;
+                //console.log(key + ' ' + value);
+            }
+            
+        }
+        // Just get English for now and stuff it in the map
+        var itext = $model.find("itext")[0];
+        var strings = $(itext).find("translation[lang*='eng']")[0];
+        fields["strings"] = strings;
+                
+        // parse the body
+        //var body = $("h:body"); xmlDoc
+        fields['xml'] = $xml;
+        //formList[reqIndex].rawText = true;
+        //formList[reqIndex].loaded = true;
+        formList[reqIndex].model = modelPrototype;
+        formList[reqIndex].loaded = true;
+        formList[reqIndex].form = fields;
+        
+        // notify the controller that the load is complete
+        reqCompleteCB(reqIndex);
+    }; 
+    
+    var cbReqTimeout = function() {
+        reqState.abort();
+        alert("URL could not be found");
+    };
+
+    Plugin.prototype.requestFormList = function (url, cb) {
+        reqCompleteCB = cb;
+        reqState.onload = this.cbReadFormList.bind(this);
+        reqState.open("get", url, true);
+        reqState.send();
+        reqTimer = setTimeout(cbReqTimeout,REQ_WAIT_TIME);
+    };
+
+    Plugin.prototype.requestForm = function (index, cb) {
+        reqCompleteCB = cb;
+        var item = this.getForm(index);
+        reqIndex = index;
+        reqState.onload = this.cbReadForm.bind(this);
+        reqState.open("get", item.url, true);
+        reqState.send();
+        reqTimer = setTimeout(cbReqTimeout,REQ_WAIT_TIME);
+    };
+
+    // A really lightweight plugin wrapper around the constructor,     
+    $.xformer = function(options) {
+        return new Plugin( this, options );
+    }
+
+})( jQuery, window, document );
