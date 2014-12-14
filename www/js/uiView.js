82303d9088649b35cc2f1cce6281bd948c069895
Tom Baker <tombaker1@gmail.com>
2014-12-14 09:41:03 -0800

    Fixed bug with topbar after popPage

--- www/js/uiView.js
+++ www/js/uiView.js
@@ -18,114 +18,109 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-;(function ( $, window, document, undefined ) {
+;
+(function ($, window, document, undefined) {
 
     function view() {
-        this.pageSet = {};          // Collection of all active pages
-        this.pageStack = [];        // Page navigation stack
+        this.pageSet = {}; // Collection of all active pages
+        this.pageStack = []; // Page navigation stack
         this.loadFormArray = [];
         this.savedFormArray = [];
-     };
+    };
 
-    view.prototype.init = function ( options ) {
+    view.prototype.init = function (options) {
         //console.log("jqm-view init");
-        
+
         // Connect to Backbone View events
         _.extend(this, Backbone.Events);
-        
+
         // Set events
-        $("#reset-dialog input[value='ok']").on("click",this.onResetOK.bind(this));
-        $("#reset-dialog input[value='cancel']").on("click",this.onResetCancel.bind(this));
-        
+        $("#reset-dialog input[value='ok']").on("click", this.onResetOK.bind(this));
+        $("#reset-dialog input[value='cancel']").on("click", this.onResetCancel.bind(this));
+
         this.on("navigate", this.changePage.bind(this));
-        
+
         // Add all of the static pages
         var pages = $("div[id^='page-']");
         for (i = 0; i < pages.length; i++) {
             var el = pages[i];
-            var name = $( pages[i] ).attr("id");
-            var page = new pageView({"name":name});
+            var name = $(pages[i]).attr("id");
+            var page = new pageView({
+                "name": name
+            });
             this.addPage(name, page);
         }
         this.pageStack.push(this.pageSet["page-home"]);
     };
-    
-    view.prototype.addPage = function ( name, page ) {
+
+    view.prototype.addPage = function (name, page) {
         if (page) {
             this.pageSet[name] = page;
-            page.on("navigate",this.changePage.bind(this));
+            page.on("navigate", this.changePage.bind(this));
         }
     };
-    
-    view.prototype.getPage = function ( name) {
+
+    view.prototype.getPage = function (name) {
         return this.pageSet[name];
     };
-    
-    view.prototype.changePage = function ( pageName ) {
+
+    view.prototype.changePage = function (pageName) {
         console.log("view changePage to " + pageName);
         if (pageName === "page-back") {
             this.popPage();
-        }
-        else if (pageName === "page-people") {
-            this.notifyModal("Not Implemented","This page is not implemented yet");
-        }
-        else {
+        } else if (pageName === "page-people") {
+            this.notifyModal("Not Implemented", "This page is not implemented yet");
+        } else {
             this.showPage(pageName);
         }
     };
-    
-    view.prototype.showPage = function ( pageName ) {
-        var element = this.pageSet[pageName];
-        if (element) {
-            var currentVisiblePage = this.pageStack[this.pageStack.length-1];
-            this.pageStack.push(element);
-            element.$el.addClass("se-page-visible");
-            $("#page-container").append(element.el);
-            element.setEvents();
-            //element.$el.foundation();
-            if (currentVisiblePage) {
-                currentVisiblePage.$el.removeClass("se-page-visible");            
-                var topBar = element.$el.find(".top-bar");
-                if (topBar) {
-                    //topBar.removeClass("expanded");
-                }
-                currentVisiblePage.$el.remove();
-            }
-           
-       }
-    };
-    
-    view.prototype.popPage = function (  ) {
-        if (this.pageStack.length >= 1) {
-            var element = this.pageStack.pop();
-            var newActive = this.pageStack[this.pageStack.length-1];
-            element.$el.removeClass("se-page-visible");
-            //element.$el.find(".top-bar").removeClase("expanded"); // close the menu
-            //newActive.$el.removeClass("se-page-left");
-            newActive.$el.addClass("se-page-visible"); 
+
+    view.prototype.showPage = function (pageName) {
+        var newActive = this.pageSet[pageName];
+        if (newActive) {
+            var currentVisiblePage = this.pageStack[this.pageStack.length - 1];
+            this.pageStack.push(newActive);
+            newActive.$el.addClass("se-page-visible");
             $("#page-container").append(newActive.el);
             newActive.setEvents();
-            element.$el.remove();
-            //newActive.$el.foundation();
-             var topBar = newActive.$el.find(".top-bar");
-                if (topBar) {
-                    topBar.removeClass("expanded");
-                }
+            if (currentVisiblePage) {
+                currentVisiblePage.$el.removeClass("se-page-visible");
+                currentVisiblePage.$el.remove();
+            }
+
         }
-        
     };
-    
-    view.prototype.newSavedFormItem = function ( options ) {
+
+    view.prototype.popPage = function () {
+        if (this.pageStack.length >= 1) {
+            var currentVisiblePage = this.pageStack.pop();
+            var newActive = this.pageStack[this.pageStack.length - 1];
+            currentVisiblePage.$el.removeClass("se-page-visible");
+            newActive.$el.addClass("se-page-visible");
+            $("#page-container").append(newActive.el);
+            newActive.setEvents();
+            currentVisiblePage.$el.remove();
+            var topBar = newActive.$el.find(".top-bar");
+            if (topBar) {
+                
+                topBar.removeClass("expanded");
+                topBar.foundation(); 
+            }
+        }
+
+    };
+
+    view.prototype.newSavedFormItem = function (options) {
         //console.log("jqm-view newSavedFormItem");
 
-        var item =  new savedFormItem(options);
+        var item = new savedFormItem(options);
         item.index = this.savedFormArray.length;
         item.render();
         this.savedFormArray.unshift(item);
         return true;
     };
-    
-    view.prototype.removeSavedFormItem = function ( options ) {
+
+    view.prototype.removeSavedFormItem = function (options) {
         //console.log("jqm-view newSavedFormItem");
         var model = options["model"];
         var item = null;
@@ -137,107 +132,104 @@
         if (!item) {
             return false;
         }
-        
+
         item.remove();
         //this.$savedFormList.listview("refresh");  TODO: saved form page not implemented
         var index = this.savedFormArray.indexOf(item);
-        this.savedFormArray.splice(index,1);
+        this.savedFormArray.splice(index, 1);
         return true;
     };
 
-    view.prototype.getModelData = function(page,model) {
+    view.prototype.getModelData = function (page, model) {
         //var form = page.model;
         //var formData = form.get("form");
         //var model = form.get("current");
         var form = app.uiController.getFormByName("disease_case");
-      var formName = form.get("name");
-      var formData = form.get("form");
-      var data = form.get("obj")["$_" + formName][0]["field"];
-      for (var key in data) {
-        var item = data[key];
-        var name = item["@name"];
-        var searchString = "#case-" + name;
-        var element = page.find(searchString).first();
-        var type = item["@type"];
-        var value = null;
-        
-        if (element.length === 0) {
-            continue;
-        }
-        
-        if (type.indexOf("reference") === 0) {
-            if (item["select"]) {
+        var formName = form.get("name");
+        var formData = form.get("form");
+        var data = form.get("obj")["$_" + formName][0]["field"];
+        for (var key in data) {
+            var item = data[key];
+            var name = item["@name"];
+            var searchString = "#case-" + name;
+            var element = page.find(searchString).first();
+            var type = item["@type"];
+            var value = null;
+
+            if (element.length === 0) {
+                continue;
+            }
+
+            if (type.indexOf("reference") === 0) {
+                if (item["select"]) {
                     var select = element.find("select").first();
                     value = select[0]["value"];
-                    model.set(name,value);
-            }
-        }
-        else {
-            if (item["select"]) {
-                var select = element.find("select").first();
-                var selectIndex = select[0].selectedIndex;
-                value = select[0]["value"];
-                model.set(name,value);
-            }
-            else {
-            switch (type) {
+                    model.set(name, value);
+                }
+            } else {
+                if (item["select"]) {
+                    var select = element.find("select").first();
+                    var selectIndex = select[0].selectedIndex;
+                    value = select[0]["value"];
+                    model.set(name, value);
+                } else {
+                    switch (type) {
                     case "string":
                         //TODO: if there is a select then get the string from the select value
                         value = (element.find("input").first().val()) || "";
-                        model.set(name,value);
+                        model.set(name, value);
                         break;
                     case "date":
                         value = (element.find("input").first().val()) || "";
-                        model.set(name,value);
+                        model.set(name, value);
                         break;
                     case "datetime":
                         value = (element.find("input").first().val()) || "";
-                        model.set(name,value);
+                        model.set(name, value);
                         break;
                     case "text":
                         value = (element.find("input").first().val()) || "";
-                        model.set(name,value);
+                        model.set(name, value);
                         break;
                     default:
                         break;
-                }
-            }
-        }
-      }
-    };
-
-    view.prototype.showForm = function(form,model,page) {
-      // Loop through keys finding page elements
-      var formName = form.get("name");
-      var formData = form.get("form");
-      var data = form.get("obj")["$_" + formName][0]["field"];
-      for (var key in data) {
-        var item = data[key];
-        var name = item["@name"];
-        var searchString = "#case-" + name;
-        var element = page.$el.find(searchString).first();
-        var type = item["@type"];
-        var value = model.get(name);
-        
-        if (element.length === 0) {
-            continue;
-        }
-        
-        if (type.indexOf("reference") === 0) {
-            if (item["select"]) {
-                //value = "";
-                var options = item["select"][0]["option"];
-                for (var i = 0; i < options.length; i++) {
-                    if (options[i]["@value"] === value) {
-                        var select = element.find("select").first();
-                        select[0].selectedIndex = i;
-                        select.selectmenu("refresh");
                     }
                 }
             }
         }
-        else {
-            switch (type) {
+    };
+
+    view.prototype.showForm = function (form, model, page) {
+        // Loop through keys finding page elements
+        var formName = form.get("name");
+        var formData = form.get("form");
+        var data = form.get("obj")["$_" + formName][0]["field"];
+        for (var key in data) {
+            var item = data[key];
+            var name = item["@name"];
+            var searchString = "#case-" + name;
+            var element = page.$el.find(searchString).first();
+            var type = item["@type"];
+            var value = model.get(name);
+
+            if (element.length === 0) {
+                continue;
+            }
+
+            if (type.indexOf("reference") === 0) {
+                if (item["select"]) {
+                    //value = "";
+                    var options = item["select"][0]["option"];
+                    for (var i = 0; i < options.length; i++) {
+                        if (options[i]["@value"] === value) {
+                            var select = element.find("select").first();
+                            select[0].selectedIndex = i;
+                            select.selectmenu("refresh");
+                        }
+                    }
+                }
+            } else {
+                switch (type) {
                 case "string":
                     //element.html(value);
                     element.find("input").first().val(value);
@@ -253,28 +245,28 @@
                     break;
                 default:
                     break;
+                }
             }
         }
-      }
 
     };
-    
-    view.prototype.notifyModal = function ( title, content ) {
+
+    view.prototype.notifyModal = function (title, content) {
         var dlg = $("#notify-dialog");
         var header = dlg.find("#header h2");
         header.html(title);
         var contentElement = dlg.find("#content h3");
         contentElement.html(content);
-        dlg.find("#ok").on("click",this.hideNotifyModal.bind(this));
+        dlg.find("#ok").on("click", this.hideNotifyModal.bind(this));
         dlg.addClass("visible");
     };
 
-    view.prototype.hideNotifyModal = function ( ) {
+    view.prototype.hideNotifyModal = function () {
         var dlg = $("#notify-dialog");
         dlg.removeClass("visible");
     };
-    
-    view.prototype.notifyMessage = function ( title, content ) {
+
+    view.prototype.notifyMessage = function (title, content) {
         var dlg = $("#notify-message");
         var header = dlg.find("#header h2");
         header.html(title);
@@ -284,47 +276,49 @@
         dlg.addClass("visible");
     };
 
-    view.prototype.hideNotifyMessage = function ( ) {
+    view.prototype.hideNotifyMessage = function () {
         var dlg = $("#notify-message");
         dlg.removeClass("visible");
     };
-    
+
     view.prototype.resetDialog = function (event) {
         var dialog = $("#reset-dialog-popup");
-        $("#reset-dialog").popup("open").popup({transition:"none"});
+        $("#reset-dialog").popup("open").popup({
+            transition: "none"
+        });
     };
-    
+
     view.prototype.onResetOK = function (event) {
         //console.log("onResetOK");
         $("#reset-dialog").popup("close");
         //app.uiController.onReset();
         app.reset();
     };
-    
+
     view.prototype.onResetCancel = function (event) {
         //console.log("onResetCancel");
         $("#reset-dialog").popup("close");
     };
-    
-    view.prototype.reset = function() {
+
+    view.prototype.reset = function () {
         // Delete new form list
         while (this.newFormArray.length) {
             var element = this.newFormArray.pop();
             element.remove();
         }
-        
+
         // Delete saved form list
         while (this.savedFormArray.length) {
             var element = this.savedFormArray.pop();
             element.remove();
         }
-        
+
         // Delete load forms list
         while (this.loadFormArray.length) {
             var element = this.loadFormArray.pop();
             element.remove();
         }
-         
+
         // reset UI lists
         this.$loadFormList.enhanceWithin();
         this.$loadFormList.controlgroup("refresh");
@@ -332,11 +326,11 @@
         //this.$savedFormList.listview("refresh");  TODO: saved form page not implemented       
     };
 
-        
+
     // bind the plugin to jQuery
     var localView = new view();
-    
+
     //$.jqmView = localView;
     app.view = localView;
 
-})( jQuery, window, document );
+})(jQuery, window, document);
